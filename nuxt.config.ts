701981ef25d2b1ba91d8eb4dfeb9605dbaac8e17
comfuture <comfuture@iplateia.com>
2025-08-06 21:20:13 +0900

    feat: add @nuxthub/core module to nuxt configuration and package dependencies

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -22,5 +22,5 @@ export default defineNuxtConfig({
 
   compatibilityDate: '2025-07-15',
   devtools: { enabled: true },
-  modules: ['@nuxtjs/tailwindcss']
+  modules: ['@nuxtjs/tailwindcss', '@nuxthub/core']
 })
