f1ff7e588279f6810e7dbaa871871faa90ca3480
comfuture <comfuture@iplateia.com>
2025-08-06 22:16:39 +0900

    refactor: simplify database initialization task by removing error handling and logging

--- server/plugins/database.ts
+++ server/plugins/database.ts
@@ -1,12 +1,6 @@
 export default defineNitroPlugin(async (nitroApp) => {
   // Run the database initialization task on startup
-  try {
-    console.log('Running database initialization task...');
+  hubHooks.hookOnce('bindings:ready', async () => {
     await runTask('db:init');
-    console.log('Database initialization task completed');
-  } catch (error) {
-    console.error('Failed to run database initialization task:', error);
-    // Don't throw the error to allow the server to start
-    // The task might have already been run before
-  }
+  })
 });
